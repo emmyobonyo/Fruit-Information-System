772c49fd397af63c07f612f392fa05b7c69a7a67
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-10-27 11:20:12 +0300

    Implement Filter Function

--- src/Redux/displayData/displayDataSlice.js
+++ src/Redux/displayData/displayDataSlice.js
@@ -1,6 +1,7 @@
 const initialState = [];
 const FETCH_DATA = 'covidTracker/displayData/FETCH_DATA';
 const FETCH_FRUIT_DATA = 'covidTracker/displayData/FETCH_FRUIT_DATA';
+const FILTER_FRUIT_DATA = 'covidTracker/displayData/FILTER_DATA';
 
 export const fetchFruitData = (fruit) => async (dispatch) => {
   const response = await fetch(`https://radiant-retreat-45087.herokuapp.com/https://www.fruityvice.com/api/fruit/${fruit}`);
@@ -32,12 +33,31 @@ export const fetchData = () => async (dispatch) => {
   dispatch({ type: FETCH_DATA, newFruits });
 };
 
+export const filterFruitData = (input) => async (dispatch) => {
+  const response = await fetch('https://radiant-retreat-45087.herokuapp.com/https://www.fruityvice.com/api/fruit/all');
+  const fruits = await response.json();
+  const newFruits = [];
+  fruits.forEach((fruit) => {
+    newFruits.push({
+      id: fruit.id,
+      name: fruit.name,
+      family: fruit.family,
+    });
+  });
+  const filteredFruits = newFruits.filter((fruit) => (
+    fruit.name.toLowerCase().includes(input.toLowerCase())
+  ));
+  dispatch({ type: FILTER_FRUIT_DATA, filteredFruits });
+};
+
 const fruitsReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_DATA:
       return action.newFruits;
     case FETCH_FRUIT_DATA:
       return action.fruitDetails;
+    case FILTER_FRUIT_DATA:
+      return action.filteredFruits;
     default:
       return state;
   }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { filterFruitData } from '../../Redux/displayData/displayDataSlice';
 import './Navigation.css';
 
 function Navigation() {
+  const dispatch = useDispatch();
+  const filterFruits = (e) => {
+    const input = e.target.value;
+    dispatch(filterFruitData(input));
+  };
   return (
     <div>
       <nav className="navigation">
@@ -10,10 +17,10 @@ function Navigation() {
           <Link to="/">
             <i className="fas fa-angle-left fa-2x" />
           </Link>
-          <h3>SPAIN</h3>
+          <h3>FRUITS</h3>
         </div>
         <div className="search-bar">
-          <input type="text" placeholder="Enter Region" />
+          <input type="text" placeholder="Enter Region" onChange={(e) => { filterFruits(e); }} />
           <i className="fas fa-angle-right fa-2x" />
         </div>
         <div className="right-navigation">
